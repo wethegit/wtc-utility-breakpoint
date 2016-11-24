7afa1f118dc6f1a5fa3807ef15511807579ca0d0
Marlon Marcello <marlon@wethecollective.com>
2016-11-24 14:52:28 -0800

    Simplified class by using Maps

--- src/es6/wtc-utility-breakpoint.js
+++ src/es6/wtc-utility-breakpoint.js
@@ -1,48 +1,32 @@
-/*
-  Get Breakpoint method
-  =======================================
-  - *Author*          liamegan
-  - *email*           liam@wethecollective.com
-  - *Created*         2014-07-22 10:36:30
-  - *namespace*       wtc.utilities
-  - *Requirements*    n/a
-  - *Description*     Returns the breakpoint that the browser is currently sized at
-  - *Edited by*       Liam Egan
-  - *Edited*          2016-06-20 11:51:50
-  - *Version*         1.0
-*/
+/**
+ * Breakpoints
+ * Simple static class to get the current breakpoint value.
+ *
+ * Usage:
+ * if(Breakpoints.current === 3) {
+ *  `Now you kow that this large`
+ * }
+ */
 
-const BREAKPOINT = {
-  S: 1,
-  M: 2,
-  L: 3,
-  XL: 4,
-  XXL: 5
-};
+/**
+ * Stores breakpoints values.
+ * @type {Object}
+ */
+let BREAKPOINT = new Map();
+BREAKPOINT.set('S', 1);
+BREAKPOINT.set('M', 2);
+BREAKPOINT.set('L', 3);
+BREAKPOINT.set('XL', 4);
+BREAKPOINT.set('XXL', 5);
 
 class Breakpoints {
-
   /**
-   * getBreakpoint
+   * Get current breakpoint value
+   *
    * @return {number} Number that correspond to the breakpoint.
   */
-  static get getBreakpoint() {
-    let bp = getComputedStyle(document.querySelector("body"), '::after').content;
-    bp = bp.replace(/'|"/gi, '');
-
-    let bps = bp.split(/,\s*/);
-    let rtn = 0;
-
-    for (let i = 0; i < bps.length; i++) {
-      bp = bps[i];
-
-      if (BREAKPOINT[bp]) {
-        rtn = BREAKPOINT[bp];
-        break;
-      }
-    }
-
-    return rtn;
+  static get current() {
+    return BREAKPOINT.get(getComputedStyle(document.querySelector("body"), '::after').content.replace(/'|"/gi, ''));
   }
 }
 
